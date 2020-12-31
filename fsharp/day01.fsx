267fcd1e22a9606fde0df610551fdf51affe909b
Vagif Abilov <vagif.abilov@nrk.no>
2020-12-31 17:53:22 +0100

    Day 1 F# solution.

---
+++ fsharp/day01.fsx
@@ -0,0 +1,32 @@
+#time "on"
+
+module DayOne =
+
+    open System
+    open System.IO
+
+    let rec findNwithSum sum n numbers accSum accNumbers =
+        if sum < 0 || n < 0 then
+            None
+        else if sum = 0 && n = 0 then
+            Some accNumbers
+        else
+            match numbers with
+            | [] -> None
+            | head :: tail ->
+                match findNwithSum (sum-head) (n-1) tail (accSum+head) (head::accNumbers) with
+                | Some result -> Some result
+                | None -> findNwithSum sum n tail accSum accNumbers
+    
+    let expenses =
+        File.ReadAllLines(__SOURCE_DIRECTORY__ + "/../data/input01.txt")
+        |> Seq.toList
+        |> List.map Int32.Parse 
+
+    findNwithSum 2020 2 expenses 0 [] 
+    |> Option.map (fun numbers -> numbers |> List.reduce (*))
+    |> Option.defaultValue 0
+
+    findNwithSum 2020 3 expenses 0 [] 
+    |> Option.map (fun numbers -> numbers |> List.reduce (*))
+    |> Option.defaultValue 0
