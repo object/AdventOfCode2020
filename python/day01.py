7533e08abdd9deba3677a863bff28caf8fd555bc
Vagif Abilov <vagif.abilov@nrk.no>
2021-01-02 11:56:55 +0100

    Day1 Python solution refactoring.

--- python/day01.py
+++ python/day01.py
@@ -6,20 +6,30 @@ with open("./data/input01.txt") as inputFile:
 numbers = [int(i.split("\n")[0]) for i in input]
 
 # Part One
+
+def findNumbersOfTwo(numbers):
+    for i in numbers:
+        for j in numbers:
+            if i + j == 2020:
+                return i * j
+
 start = time.monotonic()
-for i in numbers:
-    if 2020-i in numbers:
-        print(i * (2020-i))
-        break
+result = findNumbersOfTwo(numbers)
 stop = time.monotonic()
+print(result)
 print(stop-start)
 
 # Part Two
+
+def findNumbersOfThree(numbers):
+    for i in numbers:
+        for j in numbers:
+            for k in numbers:
+                if i + j + k == 2020:
+                    return i * j * k
+
 start = time.monotonic()
-for i in numbers:
-    for j in numbers:
-        if 2020-i-j in numbers:
-            print(i * j * (2020-i-j))
-            break
+result = findNumbersOfThree(numbers)
 stop = time.monotonic()
+print(result)
 print(stop-start)
